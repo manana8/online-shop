fd61cca6de5b2a263c36c92a84681fa521b86a48
manana8 <anastasiya.manzhueva@mail.ru>
2023-11-23 00:11:40 +0800

    The validation on the start page

--- app/public/registrate.php
+++ app/public/registrate.php
@@ -1,73 +1,77 @@
 <?php
-function validate(array $data) {
+//print_r($_SERVER);
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+if ($requestMethod === 'POST') {
+    function validate(array $data) {
 
-    $errors = [];
+        $errors = [];
 
-    if(isset($data['name'])) {
-        $name = $data['name'];
-        if (strlen($name) < 2) {
-            $errors['name'] = 'Имя должно содержать более 2 символов';
+        if(isset($data['name'])) {
+            $name = $data['name'];
+            if (strlen($name) < 2) {
+                $errors['name'] = 'Имя должно содержать более 2 символов';
+            }
+        }  else {
+            $errors['name'] = 'Введите имя';
         }
-    }  else {
-        $errors['name'] = 'Введите имя';
+
+        if(isset($data['email'])) {
+            $email = $data['email'];
+            if (strlen($email) < 4) {
+                $errors['email'] = 'Электронная почта должна содержать более 4 символов';
+            } elseif (!strpos($email, '@')) {
+                $errors['email'] = 'Некорректная почта';
+            }
+        } else {
+            $errors['email'] = 'Введите адрес электронной почты';
+        }
+
+        if(isset($data['psw'])) {
+            $password = $data['psw'];
+            if (strlen($password) < 6) {
+                $errors['psw'] = 'Пароль должен содержать 6 символов';
+            }
+        } else {
+            $errors['psw'] = 'Введите пароль';
+        }
+
+
+        if(isset($data['psw-repeat'])) {
+            $passwordRep = $data['psw-repeat'];
+            if ($password !== $passwordRep) {
+                $errors['psw-repeat'] = 'Пароли не совпадают';
+            }
+        } else {
+            $errors['psw-repeat'] = 'Введите пароль';
+        }
+
+        return $errors;
     }
 
-    if(isset($data['email'])) {
-        $email = $data['email'];
-        if (strlen($email) < 4) {
-            $errors['email'] = 'Электронная почта должна содержать более 4 символов';
-        } elseif (!strpos($email, '@')) {
-            $errors['email'] = 'Некорректная почта';
-        }
-    } else {
-        $errors['email'] = 'Введите адрес электронной почты';
+    $errors = validate($_POST);
+
+    if (empty($errors)) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['psw'];
+
+        $pdo = new PDO("pgsql:host=db;dbname=postgres", "dbuser", "dbpwd");
+
+        $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+        $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+
+        $stmt = $pdo->prepare("SELECT * FROM users");
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        print_r($data);
     }
-
-    if(isset($data['psw'])) {
-        $password = $data['psw'];
-        if (strlen($password) < 6) {
-            $errors['psw'] = 'Пароль должен содержать 6 символов';
-        }
-    } else {
-        $errors['psw'] = 'Введите пароль';
-    }
-
-
-    if(isset($data['psw-repeat'])) {
-        $passwordRep = $data['psw-repeat'];
-        if ($password !== $passwordRep) {
-            $errors['psw-repeat'] = 'Пароли не совпадают';
-        }
-    } else {
-        $errors['psw-repeat'] = 'Введите пароль';
-    }
-
-    return $errors;
-}
-
-$errors = validate($_POST);
-
-if (empty($errors)) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['psw'];
-
-    $pdo = new PDO("pgsql:host=db;dbname=postgres", "dbuser", "dbpwd");
-
-    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
-    $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-
-    $stmt = $pdo->prepare("SELECT * FROM users");
-    $stmt->execute();
-
-    $data = $stmt->fetchAll();
-
-    print_r($data);
 }
 
 ?>
 
-<form action="register_handler.php" method="POST">
+<form action="registrate.php" method="POST">
     <div class="container">
         <h1>Register</h1>
         <p>Please fill in this form to create an account.</p>
